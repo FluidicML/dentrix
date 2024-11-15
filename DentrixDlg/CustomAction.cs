ab6556cfb5ecd6776c30fc27bcf52f54fc009ef4
Joshua Potter <jrpotter2112@gmail.com>
2024-11-14 19:31:04 -0700

    Have registry key written.

--- DentrixDlg/CustomAction.cs
+++ DentrixDlg/CustomAction.cs
@@ -7,9 +7,14 @@ namespace DentrixDlg
 {
     public class CustomActions
     {
-        private const string PROPERTY = "FL_DentrixDirProperty";
+        private const string PROPERTY = "FL_DENTRIX_DIR";
         private const string DtxApiDll = "Dentrix.API.dll";
 
+        /// <summary>
+        /// Attempts to find the the Dentrix installation automatically.
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
         [CustomAction]
         public static ActionResult FL_DentrixDirSetProperty(Session session)
         {
@@ -40,7 +45,7 @@ namespace DentrixDlg
             }
 
             var dir1 = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "Dentrix");
-                
+
             if (File.Exists(Path.Combine(dir1, DtxApiDll)))
             {
                 session[PROPERTY] = dir1;
@@ -54,6 +59,25 @@ namespace DentrixDlg
             {
                 session[PROPERTY] = dir2;
             }
+
+            return ActionResult.Success;
+        }
+
+        /// <summary>
+        /// A demonstration of a deferred action with a property value set.
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        [CustomAction]
+        public static ActionResult FL_DentrixDirReadProperty(Session session)
+        {
+            // Uncomment the following to run the debugger at this point. You
+            // can use this to confirm the value of our CustomActionData is that
+            // of our `PROPERTY` set earlier.
+
+            // System.Diagnostics.Debugger.Launch();
+
+            var data = session.CustomActionData;
 
             return ActionResult.Success;
         }
